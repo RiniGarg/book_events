52790c949a99a328f528acbcc8dc7c848a00f298
RiniGarg <Rini.Garg@chronus.com>
2016-07-13 11:40:54 +0530

    intiial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookEvents::Application.config.secret_token = '8d093b2d9c0bafd0097d062dc502fa1dce99bc8f06fad79b7ca5682d201f65c9db7e1e172196f581970557988ab939086adab20c66dc8e74ee3959252410c753'
